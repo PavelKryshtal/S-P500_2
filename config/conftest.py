e8577d1c04fe705a1814e2848038f23017878064
Administrator <admin@example.com>
2022-05-31 19:38:58 +0200

    Browse233

--- config/conftest.py
+++ config/conftest.py
@@ -24,7 +24,9 @@ def setup(request, get_webdriver):
 
     driver.maximize_window()
     #time.sleep(3)
-    driver.get("https://ru.investing.com/indices/us-spx-500-chart")
+    #driver.get("https://ru.investing.com/indices/us-spx-500-chart")
+    #driver.is_visible(By.XPATH, Variables.S_p_500_accept_cookie)
+    #driver.find_element(By.XPATH, Variables.S_p_500_accept_cookie).click()
     print("\n---------------Success-----------------")
     if request.cls is not None:
         request.cls.driver = driver
--- config/test_main.py
+++ config/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 
 from config.pom import StateFunctions
 from selenium.webdriver.common.by import By
@@ -11,7 +12,12 @@ class TestBohUpdate:
 
     def test_update(self):
         library = StateFunctions(self.driver)
+        driver = self.driver
 
         # Check version
+
         library.SP500()
+        time.sleep(5)
+        driver.find_element(By.XPATH, "//button[@id = 'onetrust-accept-btn-handler']").click()
+        time.sleep(3)
         print("Success")
